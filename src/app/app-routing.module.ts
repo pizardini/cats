24c70f6b5c55ee0bd56bdc5be32876bf606e5b76
Pietro Zardini <pizardini@gmail.com>
2023-07-05 20:56:07 -0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "cats", loadChildren: () => import('./cats/cats.module').then(m => m.CatsModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/cats/cats.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewCatComponent } from './new-cat/new-cat.component';
+import { SearchCatsComponent } from './search-cats/search-cats.component';
+
+
+
+@NgModule({
+  declarations: [
+    NewCatComponent,
+    SearchCatsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SearchCatsComponent, 
+    NewCatComponent]
+})
+export class CatsModule { }
