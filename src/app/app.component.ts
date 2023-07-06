4c6bf4887b0c7846a1b7bc1d450572dcadfaa57d
Pietro Zardini <pizardini@gmail.com>
2023-07-05 22:08:24 -0300

    alternativa para routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cats';
+
+  constructor(private router: Router) {
+  
+  }
+  goToSearch(): void {
+  this.router.navigate(['cats/search'])
+  }
 }
