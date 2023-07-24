1dd2c9a8407194de44a7757e0de3be83cfb70e28
Pietro <pizardini@gmail.com>
2023-07-24 18:04:10 -0300

    tentativa de solução da api mockada

--- src/app/cats/cat.service.ts
+++ src/app/cats/cat.service.ts
@@ -5,7 +5,7 @@ import { Cat } from '../shared/models/Cat.model';
   providedIn: 'root',
 })
 export class CatService {
-  private serverUrl = 'api/cats';
+  private serverUrl = 'http://localhost:3000/data';
   constructor(private http: HttpClient) {}
   getCats() {
     return this.http.get<Cat[]>(this.serverUrl);
